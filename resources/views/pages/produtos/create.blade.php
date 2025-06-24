d6284156b1b0a1d5e9a1004091939da72125dc27
Rafaela Boldt <rrafaboldt@gmail.com>
2025-06-24 13:42:34 -0300

    import InputMask

---
+++ resources/views/pages/produtos/create.blade.php
@@ -0,0 +1,31 @@
+@extends('index')
+
+@section('content')
+
+    <form class="form" method="POST" action="{{ route('cadastrar.produto') }}">
+        @csrf
+
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Criar novo produto</h1>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Nome</label>
+            <input type="text" class="form-control @error('nome') is-invalid @enderror" name="nome" value="{{ old('nome') }}">
+            @error('nome')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Valor</label>
+            <input type="number" step="0.01" class="form-control @error('valor') is-invalid @enderror" name="valor" value="{{ old('valor') }}">
+            @error('valor')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-success">Cadastrar</button>
+    </form>
+
+@endsection
--- resources/views/pages/produtos/paginacao.blade.php
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -9,7 +9,7 @@
         <form action="{{ route('produto.index') }}" method="get">
             <input type="text" name="pesquisar" placeholder="Digite o nome">
             <button> Pesquisar </button>
-            <a href="" type="button" class="btn btn-success float-end">Incluir Produto</a>
+            <a href="{{ route('cadastrar.produto') }}" type="button" class="btn btn-success float-end">Incluir Produto</a>
         </form>
         <div class="table-responsive mt-4">
             @if ($findProduto->isEmpty())
@@ -30,7 +30,8 @@
                                 <td>{{ 'R$' . ' ' . number_format($produto->valor, 2, ',', '.') }}</td>
                                 <td>
                                     <a href="" class="btn btn-light btn-sm">Editar</a>
-                                    <a href="{{ route('produto.delete') }}" class="btn btn-danger btn-sm">Excluir</a>
+                                    <meta name="csrf-token" content="{{ csrf_token() }}">
+                                    <a onclick="deleteRegistroPaginacao('{{ route('produto.delete') }}', {{ $produto->id }})" class="btn btn-danger btn-sm">Excluir</a>
                                 </td>
                             </tr>
                         @endforeach
