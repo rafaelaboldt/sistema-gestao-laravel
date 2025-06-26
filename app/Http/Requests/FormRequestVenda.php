6484ef4407d9858d1de68aaf1932193d126ec440
Rafaela Boldt <rrafaboldt@gmail.com>
2025-06-26 17:09:56 -0300

    CRUD vendas

---
+++ app/Http/Requests/FormRequestVenda.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FormRequestVenda extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $request = [];
+        if ($this->method() == "POST" || $this->method() == "PUT") {
+            return [
+                'produto_id' => 'required',
+                'cliente_id' => 'required',
+            ];
+        }
+        return $request;
+    }
+}
---
+++ resources/views/pages/vendas/paginacao.blade.php
@@ -0,0 +1,43 @@
+{{-- Extends da index --}}
+@extends('index')
+
+@section('content')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Vendas</h1>
+    </div>
+    <div>
+        <form action="{{ route('venda.index') }}" method="get">
+            <input type="text" name="pesquisar" placeholder="Digite o nome">
+            <button> Pesquisar </button>
+            <a href="{{ route('cadastrar.venda') }}" type="button" class="btn btn-success float-end">Incluir Venda</a>
+        </form>
+        <div class="table-responsive mt-4">
+            @if ($findVenda->isEmpty())
+                <p> Não existe dados </p>
+            @else
+                <table class="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Numeração</th>
+                            <th>Produto</th>
+                            <th>Cliente</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($findVenda as $venda)
+                            <tr>
+                                <td>{{ $venda->numero_da_venda }}</td>
+                                <td>{{ $venda->produto->nome }}</td>
+                                <td>{{ $venda->cliente->nome }}</td>
+                                <td>
+                                    <a href="{{ route('atualizar.produto', $produto->id) }}" class="btn btn-light btn-sm">Enviar E-mail</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
+    </div>
+@endsection
